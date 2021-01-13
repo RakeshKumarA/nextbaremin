e3b82570df668aa8f8727de6587135cf8b3c9614
RakeshKumarA <rkakumar85@gmail.com>
2021-01-13 14:47:43 +0100

    Added Sanity

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,33 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import sanityClient from "../lib/sanity";
 
 const about = () => {
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+      title,
+      slug,
+    }`
+      )
+      .then((data) => setPostData(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <h1>This is About page</h1>
       <Link href="/">
         <a>Home page</a>
       </Link>
+      {postData.map((post) => (
+        <div key={post.slug.current}>
+          <h1>{post.title}</h1>
+          <h1>{post.slug.current}</h1>
+        </div>
+      ))}
     </>
   );
 };
